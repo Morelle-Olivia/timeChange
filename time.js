423ff4f6b9bff5f232d70b086e800c2480e2566c
Morelle-Olivia <hotoumorelle@gmail.com>
2023-03-18 22:11:51 +0100

    first commit.

---
+++ time.js
@@ -0,0 +1,8 @@
+    let seconds = new Date().getSeconds();
+    let minutes = new Date().getMinutes();
+    let hours = new Date().getHours();
+    let days = new Date().getDay();
+    let month = new Date().getMonth();
+    let time = Date();
+    console.log(month);
+    console.log(time);
